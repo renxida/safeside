617a0ff3bed185654a082ef3a0cf2be2115411cf
Xida Ren <cedar.ren@gmail.com>
2022-08-06 14:33:56 +0000

    attempt 1 at run4

---
+++ run4.sh
@@ -0,0 +1,28 @@
+#/usr/bin/env bash
+function runone{
+	files=`ls build/demos/*`
+	interval_print=$1
+	outdir=$2
+	for file in $files:
+	do
+	    if [ -f "${file}" ] && [ -r "${file}" ] && [ -x "${file}" ]; then
+		echo running $file
+		benchname=$(basename $file)
+		mkdir -p $outdir/$benchname
+		timeout 7200 perf stat -x, -a --append -o $outdir/$benchname/perf-output.csv --delay 3000 --interval-print $interval_print  -e r0CA3,r0283,r0480,r01C5 $file
+		echo donewith  $file
+	    else
+		echo $file is not a command
+	    fi
+	done
+}
+
+
+outdir=$HOME/safeside-out/
+for interval in 1 4 16 64 256 1024 4096 16384
+do
+	echo $interval
+	thisout=$outdir/interval_$interval/
+	mkdir -p  $thisout
+	runone $interval $thisout
+done
